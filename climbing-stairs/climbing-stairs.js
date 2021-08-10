c525a3a827a0739235aa245f2ace06dcbd623585
geborenik <73976680+geborenik@users.noreply.github.com>
2021-08-10 15:30:35 +0900

    Time: 72 ms (64.84%), Space: 38 MB (96.95%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  const arr = []
+ arr[1] = 1
+ arr[2] = 2
+  
+  if (n < 3) {
+    return arr[n]
+  } else {
+    for (let i = 3; i <= n; i++) {
+      arr[i] = (arr[i-1] + arr[i-2])
+    }
+    return arr[n]
+  }
+    
+};
