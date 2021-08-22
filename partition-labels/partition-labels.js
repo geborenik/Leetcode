d6798723780b8a8ccecd5d56d5cdf4e5a9c2bd3b
geborenik <73976680+geborenik@users.noreply.github.com>
2021-08-22 17:38:41 +0900

    Time: 80 ms (74.39%), Space: 40.6 MB (66.48%) - LeetHub

---
+++ partition-labels/partition-labels.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var partitionLabels = function(s) {
+  let obj = {}
+  const stringlength = [0]
+  const res = []
+  
+  for (let i = 0; i < s.length; i++) {
+    obj[s[i]] ? obj[s[i]].push(i) : obj[s[i]] = [i]
+  }
+  
+  const alphabets = Object.keys(obj)
+  let divisonPoint = obj[alphabets[0]][obj[alphabets[0]].length-1]
+  
+
+  for (let i = 1; i < alphabets.length; i++){
+    const firstIndex = obj[alphabets[i]][0]
+    const lastIndex = obj[alphabets[i]][obj[alphabets[i]].length-1]
+
+    if (lastIndex > divisonPoint && firstIndex > divisonPoint) {
+      stringlength.push(firstIndex)
+      divisonPoint = lastIndex
+    } else if (lastIndex > divisonPoint && firstIndex < divisonPoint){
+      divisonPoint = lastIndex
+    } 
+  }
+  stringlength.push(s.length)
+  
+  for (let i = 0; i < stringlength.length-1; i++) {
+    // res.push(s.slice(stringlength[i], stringlength[i+1]))
+    res.push(stringlength[i+1] - stringlength[i])
+  }
+  // console.log(stringlength,res)
+  return res
+};
